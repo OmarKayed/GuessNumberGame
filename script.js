ae3b8c654a73a2d9068a804dec2fcb3e3e490462
OmarKayed <omar2346@stud.kea.dk>
2023-09-19 15:33:00 +0200

    Finished the game

--- script.js
+++ script.js
@@ -14,21 +14,36 @@ function checkBTN() {
 checkBTN()
 
 function clear() {
-    document.getElementById("inputNumber").value = ""
-    document.getElementById("score").innerHTML = ""
+    document.getElementById("inputNumber").value
+    document.getElementById("score");
+    document.getElementById("highScore");
 }
 
 function check() {
 let inputNumber = document.getElementById("inputNumber").value;
-let score = document.getElementById("score").innerHTML;
+let score = document.getElementById("score");
+let highscore = document.getElementById("highScore");
     if (inputNumber == randomNumber) {
         document.getElementById("message").innerHTML = "Right!"
+        if (highscore.innerHTML < score.innerHTML){
+            highscore.innerHTML += 1;
+        }
     } else {
         document.getElementById("message").innerHTML = "Wrong!"
         score.innerHTML -= 1
+        if (score.innerHTML <= 0) {
+            startOver()
+        }
     }
 }
 
+// Make a function where the user can start over again with 20 points and a new random number
+function startOver() {
+    generateRandomNumber()
+    document.getElementById("message").innerHTML = "New game! Start guessing..."
+    document.getElementById("score").innerHTML = 20;
+    document.getElementById("inputNumber").value = "";
+}
 
 
 
